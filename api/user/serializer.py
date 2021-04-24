1c090f9c965999479d3d56c4b19684969edd6e6a
ZmanElete <zacharywright1@mail.weber.edu>
2021-04-24 13:36:13 -0600

    Measurement endpoint and user current user endpoint changes

--- api/user/serializer.py
+++ api/user/serializer.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import User
+
+from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 
+User = get_user_model()
 class UserSerializer(ModelSerializer):
   class Meta:
     model = User
--- api/user/view.py
+++ api/user/view.py
@@ -1,11 +1,25 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import PermissionDenied
 
 from .serializer import UserSerializer
+User = get_user_model()
 
 class UserViewSet(ModelViewSet):
   serializer_class = UserSerializer
 
   def get_queryset(self):
-    return User.objects.filter(household=self.request.user.household)
+    return User.objects.filter(id=self.request.user.id)
+
+  @action(detail=False, methods=['post'])
+  def current(self, request):
+    return super(UserViewSet, self).retrieve(request, pk=request.user.id)
+
+  def delete(self, request):
+    if request.user.is_super_user:
+      return super(UserViewSet, self).delete(request)
+    else:
+      raise PermissionDenied()
+
 
